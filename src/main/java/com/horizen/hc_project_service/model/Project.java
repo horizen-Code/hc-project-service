68b717c40e917ea93227f383c45b06aec9978f66
Samadar1 <Urapol18@gmail.com>
2025-12-10 18:15:19 +0300

    add: model Project

---
+++ src/main/java/com/horizen/hc_project_service/model/Project.java
@@ -0,0 +1,51 @@
+package com.horizen.hc_project_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "projects")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "author_id", nullable = false)
+    private UUID authorId;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "team_id")
+    private UUID teamId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Visibility visibility;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
