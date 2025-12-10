80681f3396b5df7f51503154968252271eb9e6d6
Samadar1 <urapol18@gmail.com>
2025-12-10 13:32:30 +0300

    add: start architecture

---
+++ src/main/java/com/horizen/hc_project_service/HcProjectServiceApplication.java
@@ -0,0 +1,13 @@
+package com.horizen.hc_project_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcProjectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcProjectServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/horizen/hc_project_service/HcProjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horizen.hc_project_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcProjectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
