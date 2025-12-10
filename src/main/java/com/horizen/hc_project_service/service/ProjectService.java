4b19be975728ef1662d70acd06cc0d64d205baee
Samadar1 <Urapol18@gmail.com>
2025-12-10 18:25:12 +0300

    add: interface ProjectService

---
+++ src/main/java/com/horizen/hc_project_service/service/ProjectService.java
@@ -0,0 +1,22 @@
+package com.horizen.hc_project_service.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.horizen.hc_project_service.model.Project;
+
+import java.util.Optional;
+
+public interface ProjectService {
+    List<Project> getAllPublicProjects();
+
+    Optional<Project> getProjectById(UUID id);
+
+    Optional<Project> getProjectByName(String name);
+
+    Project updateProject(UUID id, Project updatedProject, boolean isPartial);
+
+    void deleteProject(UUID id);
+
+    void transferProjectToUser(UUID projectId, UUID newOwnerId);
+}
